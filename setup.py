311ce47441302965d4bb53dbb123c0dc511b396a
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2010-02-26 02:30:46 +0900

    init

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+# encoding: utf-8
+
+from distutils.core import setup
+
+setup( name = 'wafinit',
+       version = '0.1.0',
+       description = 'Waf Initialization Tool',
+       author = 'Hideyuki Tanaka',
+       author_email = 'tanaka.hideyuki@gmail.com',
+       url = 'http://github.com/tanakh/wafinit',
+       license = 'New BSD License',
+       scripts = ['wafinit'],
+       )
